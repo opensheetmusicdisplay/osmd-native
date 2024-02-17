92b911b8e434479a1ed36fc557533755cb0473ee
Inanc <inanc@rageknuff.com>
2024-02-17 16:36:49 +0100

    refactor: single OSMD instance, extract injection scripts & message handler
     fixes #4

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,97 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyleSheet, type StyleProp, type ViewStyle } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import { osmd_min_str } from './assets/osmd_min';
 import type { IOSMDOptions } from './assets/OSMDOptions';
+import {
+  initOSMD,
+  loadMusicXML,
+  setOptions,
+} from './injection/injection_scripts';
 
 export interface OSMDProps {
-  /** the music document to render. it needs to either be a URL to a MusicXML file or a string of the MusicXML content */
+  /** The music document to render.
+   *  It needs to either be a URL to a MusicXML file or
+   *  a string of the MusicXML content
+   */
   musicXML: string;
+  /** (optional) Options to set on OSMD */
   options?: IOSMDOptions;
+  /** (optional) Custom styling to be applied to the content container */
   style?: StyleProp<ViewStyle>;
+  /** (optional) Callback that is called once the content is rendered.
+   *  Can be used to show/hide loading indicators, etc.
+   */
+  onRender?: () => void;
 }
-
-const loadOSMD = (options: IOSMDOptions, musicXML: string) => `
-var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay('osmdContainer');
-osmd.setOptions(${JSON.stringify(options)});
-window.ReactNativeWebView.postMessage('OSMD Version: ' + osmd.version);
-osmd
-  .load(${JSON.stringify(musicXML)})
-  .then(function () {
-    window.ReactNativeWebView.postMessage('starting to render...');
-    osmd.render();
-    window.ReactNativeWebView.postMessage('rendered');
-  })
-  .catch((error) => {
-    window.ReactNativeWebView.postMessage(JSON.stringify(error));
-  });
-`;
 
 export function OSMDView({
   musicXML,
-  options = {},
+  options,
   style,
+  onRender,
 }: OSMDProps): React.ReactElement {
   const webview = useRef<WebView>(null);
+
+  /** Handles messages received from the webview.
+   *  @param message a stringified JSON object (see injection scripts for how they are triggered)
+   */
+  const handleMessage = (message: string) => {
+    const data = JSON.parse(message);
+
+    // @todo: remove, debug only
+    console.log('reveived from webview:', data);
+
+    switch (data.event) {
+      /** `onInit` is called when an OSMD instance is created.
+       *  Once the instance is ready, we setup options & load music xml.
+       */
+      case 'onInit': {
+        console.log(`OSMD Version: ${data.version}`);
+
+        let js = ``;
+        if (options) js += setOptions(options);
+        js += loadMusicXML(musicXML);
+        webview.current?.injectJavaScript(js);
+
+        break;
+      }
+      /** `onRender` is called when the music sheet is rendered.
+       * A client can use this callback to react, i.e. hide a loading indicator etc.
+       */
+      case 'onRender': {
+        if (onRender) onRender();
+        break;
+      }
+    }
+  };
+
+  /** Any time options or musicXML changes, we re-inject the new js */
+  useEffect(() => {
+    if (options) {
+      webview.current?.injectJavaScript(setOptions(options));
+    }
+    webview.current?.injectJavaScript(loadMusicXML(musicXML));
+  }, [options, musicXML]);
 
   return (
     <WebView
       ref={webview}
-      originWhitelist={['*']}
       injectedJavaScript={osmd_min_str}
       source={{
         html: '<body><div id="osmdContainer"/></div></body>',
       }}
       containerStyle={style ?? styles.container}
       onLoad={() => {
-        console.log('injecting loadOSMD');
-        const js = loadOSMD(options, musicXML);
-        webview.current?.injectJavaScript(js);
+        webview.current?.injectJavaScript(initOSMD);
       }}
       onError={(syntheticEvent) => {
         const { nativeEvent } = syntheticEvent;
-        console.warn('WebView error: ', nativeEvent);
+        throw nativeEvent;
       }}
       onMessage={(event) => {
-        console.log(event.nativeEvent.data);
+        handleMessage(event.nativeEvent.data);
       }}
     />
   );
---
+++ src/injection/injection_scripts.ts
@@ -0,0 +1,24 @@
+import type { IOSMDOptions } from '../assets/OSMDOptions';
+
+/** initializes the OSMD instance within the webview. should only happen once when the webview loads.  */
+export const initOSMD = `
+var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay('osmdContainer');
+window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'onInit', version: osmd.version }));
+`;
+
+/** sets any supported IOSMDOptions   */
+export const setOptions = (options: IOSMDOptions) =>
+  `osmd.setOptions(${JSON.stringify(options)});`;
+
+/** loads the actual music xml */
+export const loadMusicXML = (musicXML: string) => `
+osmd
+  .load(${JSON.stringify(musicXML)})
+  .then(() => {
+    osmd.render();
+    window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'onRender' }));
+  })
+  .catch((error) => {
+    window.ReactNativeWebView.postMessage(JSON.stringify(error));
+  });
+`;
